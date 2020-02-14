aa91ddbcf2cc91dafd12bf68de3c2db40f4bd475
Cristina Edens <edens.cristina@gmail.com>
2020-02-14 11:45:38 -0500

    Data Fetch is working and displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Players from "./components/Players";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Players/>
     </div>
   );
 }
